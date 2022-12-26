ddde4092a5ef40cbefcb6cd7cd382e7ef8189547
Rolen Muaña <101455703+rolennn@users.noreply.github.com>
2022-12-26 23:29:00 +0800

    added page configuration to each web-page

--- pages/2_📋_General_Information.py
+++ pages/2_📋_General_Information.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+
+# page configuration
+st.set_page_config(
+    page_title="Election",
+    initial_sidebar_state="auto"
+)
+
+
 st.markdown("we put our reseach content here")
--- pages/3_📃_COMELEC_Resolutions.py
+++ pages/3_📃_COMELEC_Resolutions.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+
+# page configuration
+st.set_page_config(
+    page_title="Resolutions",
+    initial_sidebar_state="auto"
+)
+
+
 st.markdown("we put COMELEC resolutions re B&SK here")
