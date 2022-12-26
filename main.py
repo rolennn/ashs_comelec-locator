a5fbfc87b77a99f3f7af14dbedc2f9ec45c120c4
Rolen Muaña <101455703+rolennn@users.noreply.github.com>
2022-12-26 19:57:31 +0800

    edits for `main.py`
    Changes:
    * removed the option for barangay (only the municipality is relevant for the location of OEO)
    * connected `find_offices.py` to show the available offices
    * considered RAP offices (priority follows: RAP-MUN, OEO-MUN, RAP-PROV, RAP)

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 import streamlit as st
-# import numpy as np
-# import pandas as pd
-# import os
+import pandas as pd
 
-def setup_page():
+import find_offices
+
+
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def process_data():
+    offices_data = pd.read_csv("_data/offices_data.tsv", sep="\t", header=0)
+
+    return offices_data
+
+
+def setup_page(data):
+    # prepare the layout of the page by providing containers and separators
     introduction = st.container()
     st.markdown("---")
     input_location = st.container()
-    output_data = st.container()
     st.markdown("---")
-    additionals = st.container()
+    output_data = st.container()
 
+    # `introduction` contains preliminary information re. the project 
     with introduction:
         st.title("TITLE")  
-        st.markdown("Project description.")
+        st.markdown("Project description. _(feel free to suggest what to put here or on any part of the web-app, thank you -rolen)_")
     
+    # `input_location` contains a space for the user to input their location
     with input_location:
         st.info("Please input your your current place of residence to identify the nearest polling station.")
 
-        province, city, barangay = st.columns(3)
-        location, search = st.columns([7.8, 1])
+        loc = st.columns(2)
+        submit =  st.columns([1, 4, 1])
 
-        province.selectbox(
+        province = loc[0].selectbox(
             "Province",
-            ("a", "b", "c")
+            options=data["PROV"].dropna().unique()
         )
-        city.selectbox(
-            "City",
-            ("a", "b", "c")
-        )
-        barangay.selectbox(
-            "Barangay",
-            ("a", "b", "c")
+        municipality = loc[1].selectbox(
+            "Municipality",
+            options=data[data["PROV"] == province]["MUN"].unique()
         )
 
-        location.markdown("Output result here.")
-        search.button("Search")
+        submit[0].markdown("**Location:**")
+        submit[1].markdown(f"{province}, {municipality}")
+        search = submit[2].button("Find Office")
 
-    with additionals:
-        with st.expander("Notes on Privacy", expanded=False):
-            st.markdown("""
-            Additional notes
-            """)
-
-        with st.expander("Credits", expanded=False):
-            st.markdown("""
-            Additional Credits
-            """)
+    # output_data
+    with output_data:
+        if search:
+            try:
+                st.info("You may register in any of the following locations.")
+                find_offices.search(data, province, municipality)
+            except:
+                st.error("Unfortunately, we encountered difficulties searching for an office. Please try again later.")
 
 
 if __name__ == "__main__":
-    setup_page()
+    setup_page(process_data())
