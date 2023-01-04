126d5f8315c0b445ee14c45a5d61358569d0e101
Rolen Muaña <101455703+rolennn@users.noreply.github.com>
2023-01-04 11:01:07 +0800

    edited content

--- 1_🔍_Registration_Sites.py
+++ 1_🔍_Registration_Sites.py
@@ -7,8 +7,10 @@ import find_offices
 # page configuration
 st.set_page_config(
     page_title="Registration",
-    initial_sidebar_state="auto"
+    initial_sidebar_state="auto",
+    page_icon="_data/logo.jpg"
 )
+
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def process_data():
@@ -26,9 +28,13 @@ def setup_page(data):
     output_data = st.container()
 
     # `introduction` contains preliminary information re. the project 
+    text_intro = """
+    Please input your current place of residence to identify the nearest polling station. Note that the Register Anywhere Project (RAP) venues within your municipality will be prioritized (if there are any), followed by the Office of the Election Officer (OEO) for your municipality/district, followed by the RAP venues within your province (if there are any). RAP venues outside your province will be displayed last. 
+    """
+
     with introduction:
-        st.title("TITLE")  
-        st.markdown("Project description. _(feel free to suggest what to put here or on any part of the web-app, thank you -rolen)_")
+        st.subheader("Registration Sites")  
+        st.markdown(text_intro)
     
     # `input_location` contains a space for the user to input their location
     with input_location:
@@ -39,7 +45,7 @@ def setup_page(data):
 
         province = loc[0].selectbox(
             "Province",
-            options=data["PROV"].dropna().unique()
+            options=data["PROV"].unique()
         )
         municipality = loc[1].selectbox(
             "Municipality",
@@ -54,7 +60,7 @@ def setup_page(data):
     with output_data:
         if search:
             try:
-                st.info("You may register in any of the following locations.")
+                st.info("You may register in any of the following locations:")
                 find_offices.search(data, province, municipality)
             except:
                 st.error("Unfortunately, we encountered difficulties searching for an office. Please try again later.")
